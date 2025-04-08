867efd67c4f420b2271eafcdf00d3fd1b683d99a
seenutech27 <seenutech27@gmail.com>
2025-04-08 23:40:58 +0530

    Add drag and drop functionality to form builder

--- src/components/FieldOption.tsx
+++ src/components/FieldOption.tsx
@@ -1,14 +1,26 @@
-import { FC } from "react";
+import React, { FC } from "react";
+import { useDrag } from "react-dnd";
+import { DragSourceMonitor } from "react-dnd";
 
 interface FieldOptionProps {
   type: string;
 }
 
 const FieldOption: FC<FieldOptionProps> = (props) => {
+  const [{ isDragging }, dragRef] = useDrag(() => ({
+    type: "field",
+    item: { type: props.type },
+    collect: (monitor: DragSourceMonitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  }));
+
   return (
     <div
+      ref={dragRef as React.Ref<HTMLDivElement>}
       className="w-full m-1 p-2 text-center bg-gray-200 hover:bg-gray-300 cursor-grab rounded-sm
         hover:font-bold hover:shadow-sm shadow-blue-300"
+      style={{ opacity: isDragging ? 0.5 : 1 }}
     >
       {props.type}
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/authContext.tsx";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AuthProvider>
-  </StrictMode>,
+    <DndProvider backend={HTML5Backend}>
+      <AuthProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
+    </DndProvider>
+  </StrictMode>
 );
